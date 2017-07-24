f2b0d941055c7d01b7dc4afdcc92808c7a2d1536
Jason Zheng <jason.zhengxin@gmail.com>
2017-07-24 20:29:49 +1000

    revisit the retry strategy

--- main.ts
+++ main.ts
@@ -2,49 +2,26 @@
  * Created by xinzheng on 8/6/17.
  */
 import {Observable} from  "rxjs";
-import {subscribeOn} from "rxjs/operator/subscribeOn";
+import {load, loadWithFetch} from './loader';
 
-
-// let numbers = [1, 5, 10];
-
-// let source = Observable.from(numbers);
-
-// class MyObserver implements Observer<number> {
 //
-//     next(value):void {
-//         console.log(`value: ${value}`);
-//     }
+// let source = Observable.merge(
+//     Observable.of(1),
+//     Observable.from([2, 3, 4]),
+//     Observable.throw(new Error("Stop!")),
+//     Observable.of(5)
+// ).catch(e => {
+//   console.log(`caught: ${e}`);
+//   return Observable.of(10);
+// });
 //
-//     error(e):void {
-//         console.log(`error: ${e}`);
-//     }
-//
-//     complete():void {
-//         console.log("complete");
-//     }
-// }
+// source.subscribe(
+//     value => console.log(`value: ${value}`),
+//     error => console.log(`error: ${error}`),
+//     () => console.log("complete")
+// );
 
-// let Obs = new MyObserver();
 
-// let source = Observable.create(observer=>{
-//     let index = 0;
-//
-//     let produceValue = () => {
-//         observer.next(numbers[index++]);
-//
-//         if(index < numbers.length) {
-//             setTimeout(produceValue, 250);
-//         }else {
-//             observer.complete();
-//         }
-//     }
-//     produceValue();
-//
-// }).map(n=> n * 2)
-//     .filter(n => n > 4);
-
-// observe event
-// let circle = document.getElementById("circle");
 let output = document.getElementById("output");
 let button = document.getElementById("button");
 
@@ -58,41 +35,6 @@ let click = Observable.fromEvent(button, "click");
     // .filter(value => value.x < 5)
     // .delay(300);
 
-// function onNext(value) {
-//     circle.style.left = value.x;
-//     circle.style.top = value.y;
-
-function load(url: string) {
-    return Observable.create(observer => {
-        let xhr = new XMLHttpRequest();
-
-        xhr.addEventListener("load", () => {
-            if(xhr.status === 200) {
-                let data = JSON.parse(xhr.responseText);
-                observer.next(data);
-                observer.complete();
-            } else {
-                observer.error(xhr.statusText);
-            }
-
-        });
-
-        xhr.open("GET", url);
-        xhr.send();
-    }).retryWhen(retryStrategy());
-}
-
-function retryStrategy() {
-    return function (errors) {
-        return errors
-            .scan((acc, value)=>{
-                console.log(acc, value);
-                return acc + 1;
-            }, 0)
-            .takeWhile(acc => acc < 4)
-            .delay(1000);
-    }
-}
 
 function renderMovies(movies) {
     movies.forEach(m=>{
@@ -102,16 +44,16 @@ function renderMovies(movies) {
     })
 }
 
-click.flatMap(e=>load("movies.json"))
+loadWithFetch("movies.json").subscribe(renderMovies,
+  e => console.log(`error: ${e}`),
+    () => console.log("complete!"));
+
+
+click.flatMap(e=>loadWithFetch("moviess.json"))
     .subscribe(
         renderMovies,
         e => console.log(`error: ${e}`),
         () => console.log("complete")
     );
 
-// click.subscribe(
-//     e => load("movies.json"),
-//     // value => console.log(value),
-//     e => console.log(`error: ${e}`),
-//     () =>  console.log("complete")
-// );
+
